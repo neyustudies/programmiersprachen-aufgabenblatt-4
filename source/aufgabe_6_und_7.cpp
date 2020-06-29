fa1cec2465920b5d7911bdbb5693f97d5f50009c
neyustudies <science-cat@gmx.net>
2020-06-29 11:48:23 +0200

    task 4.7 in aufgabe_6_und_7.cpp

--- source/aufgabe_6_und_7.cpp
+++ source/aufgabe_6_und_7.cpp
@@ -3,10 +3,14 @@
 #include "circle.hpp"
 #include <list>
 
-
-TEST_CASE("Sorting Circles with Lambda", "[sort_lambda]") {
+/* task 4.6 */
+TEST_CASE("Sorting Circles with Lambda", "[sorting_circles] (lambda)") {
     
   std::list<Circle> sorted_circles;
+
+  auto sorting_lambda = [] (Circle const& a, Circle const& b) -> bool {
+    return (a.radius() < b.radius());
+  };
 
   SECTION("radius of circles should be in correct order after sorting") {
     sorted_circles.push_back(Circle(Vec2{300, 300}, 30));
@@ -15,15 +19,35 @@ TEST_CASE("Sorting Circles with Lambda", "[sort_lambda]") {
     sorted_circles.push_back(Circle(Vec2{200, 200}, 10));
     sorted_circles.push_back(Circle(Vec2{100, 100}, 70));
 
-
-    auto sorting_lambda = [] (Circle const& a, Circle const& b) -> bool {
-      return (a.radius() < b.radius());
-    };
-
     REQUIRE_FALSE(std::is_sorted(sorted_circles.begin(), sorted_circles.end()));
     sorted_circles.sort(sorting_lambda);
+    REQUIRE(std::is_sorted(sorted_circles.begin(), sorted_circles.end()));    
+  }
+}
+
+
+/* task 4.7 optional */
+TEST_CASE("Sorting circles with Functor", "[sorting_circles] (functor)") {
+
+  std::list<Circle> sorted_circles;
+  
+  class less {
+    public:
+      bool operator() (Circle const& a, Circle const& b) const {
+        return a.radius() < b.radius();
+      }
+  };
+
+  SECTION("radius of circles should be in correct order after sorting") {
+    sorted_circles.push_back(Circle(Vec2{300, 300}, 30));
+    sorted_circles.push_back(Circle(Vec2{400, 400}, 20));
+    sorted_circles.push_back(Circle(Vec2{400, 400}, 40));
+    sorted_circles.push_back(Circle(Vec2{200, 200}, 10));
+    sorted_circles.push_back(Circle(Vec2{100, 100}, 70));
+
+    REQUIRE_FALSE(std::is_sorted(sorted_circles.begin(), sorted_circles.end()));
+    sorted_circles.sort(less{});
     REQUIRE(std::is_sorted(sorted_circles.begin(), sorted_circles.end()));
-    
   }
 }
 
@@ -31,5 +55,4 @@ TEST_CASE("Sorting Circles with Lambda", "[sort_lambda]") {
 int main(int argc, char* argv[]) {
   return Catch::Session().run(argc, argv);
 }
-
 
