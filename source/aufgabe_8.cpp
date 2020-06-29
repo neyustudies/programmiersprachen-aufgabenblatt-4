6bfc7135510dc044aea3ccc8e5d90d58352e594a
neyustudies <science-cat@gmx.net>
2020-06-29 13:56:27 +0200

    task 4.8 indent changes

--- source/aufgabe_8.cpp
+++ source/aufgabe_8.cpp
@@ -11,38 +11,51 @@ int main() {
   container kann 10 ints aufnehmen */
   std::vector<int>v_0(10);
   
-  /* generiert 10 ints und speichert sie in v_0 */
+  /* generiert 10 ints 
+  und speichert sie in v_0 */
   for(auto& v : v_0) {
     v=std::rand();
   }
 
-  /* mittels const iterator werden alle Elemente aus v_0 in den 
-  ostream_iterator kopiert und der Reihe nach pro Zeile ausgegeben */
+  /* mittels const iterator werden alle 
+  Elemente aus v_0 in den ostream_iterator 
+  kopiert und der Reihe nach pro Zeile ausgegeben */
   std::copy(std::cbegin(v_0), std::cend(v_0), 
   std::ostream_iterator<int>(std::cout, "\n"));
 
-  /* die Elemente aus v_0 werden in einer Liste namens l_0 gespeichert */
+
+  /* die Elemente aus v_0 werden 
+  in einer Liste namens l_0 gespeichert */
   std::list<int> l_0(std::cbegin(v_0), std::cend(v_0));
 
-  /* die Elemente aus l_0 werden in einer Liste namens l_1 gespeichert */
+
+  /* die Elemente aus l_0 werden 
+  in einer Liste namens l_1 gespeichert */
   std::list<int> l_1(std::cbegin(l_0), std::cend(l_0));
 
-  /* die Reihenfolge der Elemente in l_1 wird 
-  umgedreht und (wie oben) ausgegeben */
+
+  /* die Reihenfolge der Elemente in l_1 
+  wird umgedreht und (wie oben) ausgegeben */
   std::reverse(std::begin(l_1), std::end(l_1));
   std::copy(std::cbegin(l_1), std::cend(l_1), 
   std::ostream_iterator<int>(std::cout, "\n"));
 
-  /* die Elemente von l_1 werden mittels der 
-  Memberfunktion list::sort() sortiert und ausgegeben */
+
+  /* die Elemente von 
+  l_1 werden mittels der 
+  Memberfunktion list::sort() 
+  sortiert und ausgegeben */
   l_1.sort();
   std::copy(l_1.cbegin(), l_1.cend(), 
   std::ostream_iterator<int>(std::cout, "\n"));
 
-  /* mittels std::generate wird der vector v_0 mit Zufallszahlen gefüllt,
-  die die vorherigen Zahlen überschrieben, dann wieder ausgegeben */
+
+  /* mittels std::generate wird der vector v_0 
+  mit Zufallszahlen gefüllt, die die vorherigen 
+  Zahlen überschrieben, dann wieder ausgegeben */
   std::generate(std::begin(v_0), std::end(v_0), std::rand);
-  std::copy(v_0.crbegin(), v_0.crend(), std::ostream_iterator<int>(std::cout, "\n"));
+  std::copy(v_0.crbegin(), v_0.crend(), 
+  std::ostream_iterator<int>(std::cout, "\n"));
 
   return 0;
 } 
