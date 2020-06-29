c932adfa00bb41317e2a55aa7a0d81747ccb7ed5
neyustudies <science-cat@gmx.net>
2020-06-29 10:08:31 +0200

    task 4.5 create tests for sorted_circles

--- source/aufgabe_5.cpp
+++ source/aufgabe_5.cpp
@@ -4,11 +4,31 @@
 #include <list>
 #include <algorithm>
 
-int main() {
+  TEST_CASE("Sorting Circles ", "[sort]") {
+    
+    std::list<Circle> sorted_circles;
 
-  std::list<Circle> sorted_circles;
+    SECTION("radius of circles should be in correct order after sorting") {
+      sorted_circles.push_back(Circle(Vec2{300, 300}, 30));
+      sorted_circles.push_back(Circle(Vec2{400, 400}, 20));
+      sorted_circles.push_back(Circle(Vec2{400, 400}, 40));
+      REQUIRE_FALSE(std::is_sorted(sorted_circles.cbegin(), sorted_circles.cend()));
+      sorted_circles.sort();
+      REQUIRE(std::is_sorted(sorted_circles.cbegin(), sorted_circles.cend()));
+      sorted_circles.push_back(Circle(Vec2{200, 200}, 10));
+      sorted_circles.push_back(Circle(Vec2{100, 100}, 70));
+      REQUIRE_FALSE(std::is_sorted(sorted_circles.cbegin(), sorted_circles.cend()));
+      sorted_circles.sort();
+      REQUIRE(std::is_sorted(sorted_circles.cbegin(), sorted_circles.cend()));
+    }
+    
+    
+
+  }
 
 
-  return 0;
-}
+  int main(int argc, char* argv[]) {
+    return Catch::Session().run(argc, argv);
+  }
+
 
