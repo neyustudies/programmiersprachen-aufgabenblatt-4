d607ad3074b0b032893b4cd3ce652c044707a135
neyustudies <science-cat@gmx.net>
2020-07-02 08:33:35 +0200

    update aufgabe_13.cpp
    - indent changes
    - name correction

--- source/aufgabe_13.cpp
+++ source/aufgabe_13.cpp
@@ -6,17 +6,18 @@
 
 
 std::vector<Circle> circles{{5.0f},{3.0f},{8.0f},{1.0f},{5.0f}};
-std::vector <Circle> big_circles(circles.size());
-auto is_bigger = [](Circle c) -> bool {return c.radius() > 4;};
+std::vector <Circle> large_circles(circles.size());
+auto is_larger = [](Circle c) -> bool {return c.radius() > 4.0f;};
 
 
-TEST_CASE("Copy Circles with radius > 4.0f", "[bigger_radius]") {
-  auto it = std::copy_if(circles.cbegin(),circles.cend(),big_circles.begin(),is_bigger);
-  big_circles.resize(std::distance(big_circles.begin(), it));
-  REQUIRE (std::all_of(big_circles.begin(), big_circles.end(), is_bigger));
+TEST_CASE("Copy Circles with radius > 4.0f", "[larger_radius]") {
+  auto it = std::copy_if(circles.cbegin(), circles.cend(), large_circles.begin(), is_larger);
+  large_circles.resize(std::distance(large_circles.begin(), it));
+  REQUIRE (std::all_of(large_circles.begin(), large_circles.end(), is_larger));
 }
 
 
 int main(int argc, char* argv[]) {
   return Catch::Session().run(argc, argv);
 }
+
