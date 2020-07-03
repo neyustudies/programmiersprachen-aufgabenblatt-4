2129fd82a746e319ed06ae505eadd21c8223df5d
neyustudies <science-cat@gmx.net>
2020-07-03 15:57:13 +0200

    merge aufgabe_10 and aufgabe_11

--- source/aufgabe_10_und_11.cpp
+++ source/aufgabe_10_und_11.cpp
@@ -1,8 +1,29 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
-#include <vector>
+#include <cmath>
 #include <algorithm>
 
+/* task 4.10 */
+
+/* helper function */
+bool is_multiple_of_3(unsigned int i) {return (i % 3) == 0;}
+
+TEST_CASE("filter alle vielfache von drei", "[erase]") {
+  std::srand((int)time(0));
+  std::vector<unsigned int> v(100);
+  std::generate(v.begin(), v.end(), []() -> bool {return rand() % 101;});
+
+  /* removes all elements which are not multiples of 3 */
+  v.erase(std::remove_if(v.begin(), v.end(), [](unsigned int i) {
+    return !is_multiple_of_3(i);}), v.end());
+
+  REQUIRE(std::all_of(v.begin(), v.end(), is_multiple_of_3));
+}
+
+
+/* --------------------------------------------------------------------------------------- */
+
+/* task 4.11 */
 std::vector<int> v_1{1, 2, 3, 4, 5, 6, 7, 8, 9};
 std::vector<int> v_2{9, 8, 7, 6, 5, 4, 3, 2, 1};
 std::vector<int> v_3(9);
@@ -16,7 +37,9 @@ TEST_CASE("add up two vectors, save result in another vector", "[transform]") {
   REQUIRE(std::all_of(v_3.cbegin(), v_3.cend(), [](int a) ->bool {return a == 10;}));
 }
 
+
 int main(int argc, char* argv[]) {
   return Catch::Session().run(argc, argv);
 }
+
 
