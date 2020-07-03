9c6a0f043021f7b26997ced41f5d29539051aa62
neyustudies <science-cat@gmx.net>
2020-07-03 15:57:36 +0200

    merge aufgabe_12 and aufgabe_13

--- source/aufgabe_12_und_13.cpp
+++ source/aufgabe_12_und_13.cpp
@@ -1,10 +1,11 @@
 #define CATCH_CONFIG_RUNNER
-#include "catch.hpp"
+#include "circle.hpp"
+#include <catch.hpp>
 #include <vector>
 #include <list>
 #include <set>
 
-
+/* task 4.12 */
 bool is_even(int n) {return n % 2 == 0;}
 
 template<typename T, typename P>
@@ -47,7 +48,21 @@ TEST_CASE("should return even numbers (random)", "[random]") {
 }
 
 
+/* ------------------------------------------------------------------------------------------ */
+
+/* task 4.13 */
+std::vector<Circle> circles{{5.0f},{3.0f},{8.0f},{1.0f},{5.0f}};
+std::vector <Circle> large_circles(circles.size());
+auto is_larger = [](Circle c) -> bool {return c.radius() > 4.0f;};
+
+
+TEST_CASE("Copy Circles with radius > 4.0f", "[larger_radius]") {
+  auto it = std::copy_if(circles.cbegin(), circles.cend(), large_circles.begin(), is_larger);
+  large_circles.resize(std::distance(large_circles.begin(), it));
+  REQUIRE (std::all_of(large_circles.begin(), large_circles.end(), is_larger));
+}
+
+
 int main(int argc, char* argv[]) {
   return Catch::Session().run(argc, argv);
 }
-
